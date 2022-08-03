c29199f634fa873c9efb8cdf36c2999951f7417d
Scott <sfitzpatrick55@gmail.com>
2022-08-03 19:42:31 -0400

    Create glove views Create and Index

---
+++ GloveYourself.Models/Glove/GloveCreate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GloveYourself.Models.Glove
+{
+    public class GloveCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(200, ErrorMessage = "Sorry, there are too many characters in this field.")]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Brand { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+    }
+}
+
--- GloveYourself.WebMVC/Controllers/GloveController.cs
+++ GloveYourself.WebMVC/Controllers/GloveController.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GloveYourself.Models.Glove;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace GloveYourself.WebMVC.Controllers
 {
+    [Authorize]
     public class GloveController : Controller
     {
+        //
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            var model = new GloveListItem[0];
+
+            return View();
+        }
+
+        //
+        // GET: /glove/create
+        public IActionResult Create()
         {
             return View();
         }
